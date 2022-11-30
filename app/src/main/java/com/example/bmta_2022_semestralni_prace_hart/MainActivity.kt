693f14651164d3cc511587849b7f421829a02e00
st64515 <st64515@upce.cz>
2022-11-30 23:24:39 +0100

    hotovy vertikalni layout MainActivity

--- app/src/main/java/com/example/bmta_2022_semestralni_prace_hart/MainActivity.kt
+++ app/src/main/java/com/example/bmta_2022_semestralni_prace_hart/MainActivity.kt
@@ -10,12 +10,15 @@ import com.journeyapps.barcodescanner.ScanOptions
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        getSupportActionBar()?.hide()
+        this.window.statusBarColor = this.resources.getColor(R.color.blue_darker2)
 
         binding.buttonScan.setOnClickListener()
         {
